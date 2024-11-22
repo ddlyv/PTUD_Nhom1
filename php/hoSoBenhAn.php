05f7fdff59f1693c8e842243460835e1bfc69220
MinhKong-23 <nguyenminhcong12d1@gmail.com>
2024-11-22 16:29:28 +0700

    test phân trang

--- php/hoSoBenhAn.php
+++ php/hoSoBenhAn.php
@@ -1,5 +1,10 @@
-<?php include 'header.php'; ?>
-
+    <?php 
+        include 'header.php'; 
+    ?>
+    <?php
+        include 'myclass/clsbenhnhan.php';
+        $p=new benhnhan();
+    ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/giaodienmc.css">
     <div class="section1">
@@ -11,7 +16,6 @@
             <input type="text" id="searchInput" placeholder="Tìm kiếm..." class="form-control h-25 d-inline-block" style="width: auto; display: inline-block;">
             <button id="searchButton" class="btn btn-primary">Tìm kiếm</button>
         </div>
-
         <!-- <div class="date-filter" align="right" style="margin-bottom: 20px;">
             <label for="startDate" class="form-label">Từ ngày:</label>
             <input type="date" id="startDate" class="form-control d-inline-block" style="width: auto; display: inline-block; margin-right: 10px;">
@@ -21,7 +25,7 @@
             
             <button id="filterButton" class="btn btn-primary">Lọc</button>
         </div> -->
-        <table class="table table-hover">
+        <!-- <table class="table table-hover">
             <thead>
             <tr>
                 <th>STT</th>
@@ -44,7 +48,44 @@
                 <td>john@example.com</td>
             </tr>
             </tbody>
-        </table>
+        </table> -->
+        <?php
+            $id_benhnhan = $_SESSION['id'];
+            //số phần tử mỗi trang
+            $inf_per_page=!empty($_REQUEST['per_page'])?$_REQUEST['per_page']:5;
+            // đặt trang gốc
+            $current_page=!empty($_REQUEST['page'])?$_REQUEST['page']:1;
+            // công thức tính cho offset (bắt đầu từ 0)
+            $offset=($current_page-1)*$inf_per_page;
+            $totalsql=mysqli_query($p->connectdb(),"SELECT *
+                                FROM hoSoBenhAn
+                                JOIN benhnhan ON hoSoBenhAn.maBenhNhan = benhnhan.maBenhNhan
+                                JOIN bacsi ON hoSoBenhAn.maBacSi = bacsi.maBacSi
+                                WHERE benhnhan.maTaiKhoan = $id_benhnhan");
+            $totalsql=$totalsql->num_rows;
+            //ceil làm tròn 1.5 ==> 2
+            $totalpages=ceil($totalsql/$inf_per_page);
+            //Hàm này sẽ in ra giá trị của biến $totalsql, cho phép bạn xem số lượng hồ sơ bệnh án đã tìm thấy.
+            //var_dump($totalsql);exit;
+
+            $p->xemHoSoBenhAn("SELECT 
+                                    CONCAT(benhnhan.hoTenDem, ' ', benhnhan.ten) AS hoTenBenhNhan,
+                                    CONCAT(bacsi.hoTenDem, ' ', bacsi.ten) AS hoTenBacSi,
+                                    hoSoBenhAn.chuanDoan,
+                                    hoSoBenhAn.ketLuan,
+                                    hoSoBenhAn.trangThai,
+                                    hoSoBenhAn.ngayTaoHoSo
+                                FROM hoSoBenhAn
+                                JOIN benhnhan ON hoSoBenhAn.maBenhNhan = benhnhan.maBenhNhan
+                                JOIN bacsi ON hoSoBenhAn.maBacSi = bacsi.maBacSi
+                                WHERE benhnhan.maTaiKhoan = $id_benhnhan
+                                ORDER BY hoSoBenhAn.ngayTaoHoSo DESC limit $inf_per_page offset $offset;
+                            ",$current_page,$inf_per_page);
+    
+        ?>
+        
+        <div class="pagination"><?php include 'pagination.php';?></div> <!-- phân trang -->
+        
         <div class="button">
                 <input type="submit" name="nut" id="backButton" value="Quay lại">
                 <input type="submit" name="nut" id="finishButton" value="Xong">
--- php/myclass/clsbenhnhan.php
+++ php/myclass/clsbenhnhan.php
@@ -1,7 +1,56 @@
 <?php
     include('clsbenhvien.php');
-    class benhnhan extends benhnhan
+    class benhnhan extends hospital
     {
+        public function  xemHoSoBenhAn($sql,$current_page,$inf_per_page)
+        {
+            $link=$this->connectdb();
+            $ketqua=mysqli_query($link,$sql);
+            if(mysqli_num_rows($ketqua)>0)
+            {
+                echo'<table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Họ tên</th>
+                            <th>Bác sĩ khám</th>
+                            <th>Chuẩn đoán</th>
+                            <th>Kết luận</th>
+                            <th>Trạng thái</th>
+                            <th>Ngày tạo</th>
+                        </tr>
+                        </thead>
+                        <tbody>';
+                $count=1 + ($current_page - 1) * $inf_per_page;;
+                while($row=mysqli_fetch_array($ketqua))
+                {
+                    $maHoSo=$row['maHoSo'];
+                    $chuanDoan=$row['chuanDoan'];
+                    $ketLuan=$row['ketLuan'];
+                    $trangThai=$row['trangThai'];
+                    $ngayTaoHoSo=$row['ngayTaoHoSo'];
+                    $maBenhNhan=$row['maBenhNhan'];
+                    $maBacSi=$row['maBacSi'];
+                    echo'<tr>
+                            <td>'.$count.'</td>
+                            <td>'.$row['hoTenBenhNhan'].'</td>
+                            <td>'.$row['hoTenBacSi'].'</td>
+                            <td>'.$chuanDoan.'</td>
+                            <td>'.$ketLuan.'</td>
+                            <td>'.$trangThai.'</td>
+                            <td>'.$ngayTaoHoSo.'</td>
+                        </tr>';
+                    $count++;
+                }
+                echo'</tbody>
+                    </table>';
+            }
+            else {
+                echo "Không có hồ sơ nào.";
+            }
+        }
+
+        
         
     }
 ?>
