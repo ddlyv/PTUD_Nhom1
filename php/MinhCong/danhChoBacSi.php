2c23e15d39d75a348966294418fac433e23455f9
dnlym <95546645+dnlym@users.noreply.github.com>
2024-12-12 11:49:27 +0700

    Update

--- php/MinhCong/danhChoBacSi.php
+++ php/MinhCong/danhChoBacSi.php
@@ -25,7 +25,34 @@
                     <p>Xem lịch làm</p>
                 </a>
             </div>
-            
+            <div class="grid-item">
+                <a href="../bacsi/KhamBenh.php">
+                    <i class="fa-solid fa-heart"></i>
+                    <p>Khám bệnh</p>
+                </a>
+            </div>
+
+            <div class="grid-item">
+                <a href="../bacsi/HoSoBenhAn.php">
+                    <i class="fa-solid fa-heart"></i>
+                    <p>Hồ Sơ Bệnh Án</p>
+                </a>
+            </div>
+
+            <div class="grid-item">
+                <a href="../bacsi/XemLichTaiKham.php">
+                    <i class="fa-solid fa-heart"></i>
+                    <p>Lịch tái khám</p>
+                </a>
+            </div>
+
+            <div class="grid-item">
+                <a href="../bacsi/ThemLichLam.php">
+                    <i class="fa-solid fa-heart"></i>
+                    <p>Đăng ký lịch làm</p>
+                </a>
+            </div>
+        </div>
         </div>
     </div>
 
